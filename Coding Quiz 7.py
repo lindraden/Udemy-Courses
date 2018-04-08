e3252e5539802841f680c2f2f6845e250d4731af
lindraden <fluffkey@gmail.com>
2018-04-08 12:08:52 +1000

    Coding Quiz 7

---
+++ Coding Quiz 7.py
@@ -0,0 +1,16 @@
+# Create a variable called name and set it equal to your name
+name = "Nathan"
+
+# Create a variable called age and set it to your age in years 
+# Note: This must be an int, not a string!
+age = 27
+
+# Create a variable called place and set it equal to where you live
+place = "Blacktown"
+
+# Create a variable called output, and use string formatting to 
+# make it like the example text in the description
+output = "Hello my name is {} and I am {} years old. I live in {} and I love Python!".format(name, age, place)
+
+# Finally, use the print() function to print the output.
+print(output)
